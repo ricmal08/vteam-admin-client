8256c3270a125db034a6cb5ea181389e27afe02e
Ricmal08 <malmgren.kontakt@gmail.com>
2025-11-11 23:16:39 +0100

    feat: setup initial app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
+import React from 'react';
+import { Box, Heading, VStack } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Userinterface from './components/interface/userinterface.jsx';
+import Accounts from './views/accounts.jsx';
+import Units from './views/units.jsx';
+
+const API_URL = "http://localhost:5173"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <BrowserRouter>
+      <Routes>
+       
+        <Route path="/" element={<Userinterface />}>
+
+          {/*Dashboard-komponent
+          <Route index element={<Dashboard />} />*/}
+
+          <Route path="overview" element={<Accounts apiUrl={API_URL} />} />
+
+          <Route path="units" element={<Units apiUrl={API_URL} />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
