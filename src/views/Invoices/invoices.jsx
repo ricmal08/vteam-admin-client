c2a2225d588c372d99196c67242d9a72951af77c
Ricmal08 <malmgren.kontakt@gmail.com>
2025-11-28 12:48:24 +0100

    feat: start implementation of invoices view

--- src/views/Invoices/invoices.jsx
+++ src/views/Invoices/invoices.jsx
@@ -4,17 +4,46 @@ import { useParams, useNavigate } from 'react-router-dom';
 import './invoices.css';
 import { API_URL } from "../../config.js"
 
-function Invoices({ apiUrl }) {
+function Invoices() {
+  const [invoices, setInvoices] = useState([]);
 
      useEffect(() => {
-}, [apiUrl])
+      const fetchInvoices = async () => {
+
+    try {
+
+          const response = await fetch(`${API_URL}/api/invoices`);
+
+          if (!response.ok) {
+          throw new Error(`Något gick fel, status: ${response.status}. Mer info: ${errorData.message}`);
+        }
+
+        const data = await response.json();
+        setInvoices(data);
+          // TODO: ordna eventuellt fler kontroller
+        } catch (err) {
+          console.error("Ett fel inträffade vid fetch:", err);
+         
+        }
+    };
+    fetchInvoices();
+}, [])
  return (
 
 <div className="invoices-container">
 
       <h2 className="invoices-title">Fakturor</h2>
 
-      <p>Här kommer befintliga fakturor att synas.</p>
+      {invoices.length > 0 ? (
+        <ul>
+          {invoices.map(invoice => (
+
+            <li key={invoice._id}>{invoice.email}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Inga fakturor kunde hittas.</p>
+      )}
     </div>
   );
 }
