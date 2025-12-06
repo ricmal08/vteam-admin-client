c378ed3c3db539b813da03a96c6c52390b24c03a
Ricmal08 <malmgren.kontakt@gmail.com>
2025-12-06 22:42:01 +0100

    feat: added static column to main layout

--- src/components/interface/userinterface.jsx
+++ src/components/interface/userinterface.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-//import { Box, Heading, VStack, Link } from '@chakra-ui/react';
 import { Link as RouterLink, Outlet } from 'react-router-dom';
+import './userinterface.css'; 
 
 function Userinterface() {
   return (
@@ -16,6 +16,31 @@ function Userinterface() {
       <main>
         <Outlet />
       </main>
+      <div className="main-container">
+        <nav className="nav-menu">
+          <h2 className="nav-title">Admin-Client</h2>
+          <h3> Innehåll: </h3>
+          <RouterLink className="nav-link" to="/">Översikt</RouterLink>
+          <RouterLink className="nav-link" to="/overview">Användare</RouterLink>
+          {/*<RouterLink className="nav-link" to="/admins">Administratörer</RouterLink>*/ }
+          <RouterLink className="nav-link" to="/units">Fordon</RouterLink>
+          <RouterLink className="nav-link" to="/invoices">Fakturor</RouterLink>
+        </nav>
+        <main className="content-container">
+          <Outlet />
+        </main>
+
+        <aside className="layout-searchbar">
+          <div className="layout-box">
+            <h3>Sök:</h3>
+            <p>Kundnummer:</p>
+            <p>Kontonummer:</p>
+            <p>Registreringsnummer:</p>
+            <p>Fakturanummer:</p>
+          </div>
+        </aside>
+
+      </div>
     </div>
   );
 }
