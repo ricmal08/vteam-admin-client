deb6165fb22a17d0fc3b33f7069a1698577a6159
Ricmal08 <malmgren.kontakt@gmail.com>
2025-12-29 21:54:24 +0100

    chore: code-cleanup

--- src/components/interface/userinterface.jsx
+++ src/components/interface/userinterface.jsx
@@ -4,43 +4,41 @@ import './userinterface.css';
 
 function Userinterface(currentUser) {
 
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.removeItem('accessToken');
     navigate('/login');
   };
 
-  return (
-    <div className="layout-container">
-      <nav className="nav-menu">
-        <h2 className="nav-title">Admin-Client</h2>
-            <button onClick={handleLogout} className="logout-button">Logga ut</button>
-        <h3> Innehåll: </h3>
+return (
+  <div className="layout-container">
+    <nav className="nav-menu">
+      <h2 className="nav-title">Admin-Client</h2>
+          <button onClick={handleLogout} className="logout-button">Logga ut</button>
+      <h3> Innehåll: </h3>
 
-      {!currentUser && (
-      <RouterLink className="nav-link" to="/login">Logga in</RouterLink>
+    {!currentUser && (
+    <RouterLink className="nav-link" to="/login">Logga in</RouterLink>
+    )}
+
+    {currentUser && (
+      <>
+      <RouterLink className="nav-link" to="/map">Karta</RouterLink>
+      <RouterLink className="nav-link" to="/users">Användare</RouterLink>
+      <RouterLink className="nav-link" to="/bikes">Cyklar</RouterLink>
+      <RouterLink className="nav-link" to="/cities">Städer</RouterLink>
+      <RouterLink className="nav-link" to="/invoices">Fakturor</RouterLink>
+      <RouterLink className="nav-link" to="/admins">Administratörer</RouterLink>
+      </>
       )}
-
-      {currentUser && (
-        <>
-        <RouterLink className="nav-link" to="/">Översikt</RouterLink>
-        <RouterLink className="nav-link" to="/users">Användare</RouterLink>
-        <RouterLink className="nav-link" to="/bikes">Cyklar</RouterLink>
-        <RouterLink className="nav-link" to="/cities">Städer</RouterLink>
-        <RouterLink className="nav-link" to="/invoices">Fakturor</RouterLink>
-        <RouterLink className="nav-link" to="/admins">Administratörer</RouterLink>
-        <RouterLink className="nav-link" to="/map">Karta</RouterLink>
-        </>
-        )}
-      </nav>
-      <main className="content-container">
-        <Outlet />
-      </main>
+    </nav>
+    <main className="content-container">
+      <Outlet />
+    </main>
 
 
-      </div>
-  
+  </div>
   );
 }
 
--- src/views/Cities/city.jsx
+++ src/views/Cities/city.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import './city.css';
 import { apiRequest } from '../../api/api.js';
 
@@ -12,84 +10,73 @@ function City() {
   const [bikes, setBikes] = useState([]); 
   const [error, setError] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
 
-      if(!cityId) return
+    if(!cityId) return
 
-      const fetchCity = async () => {
+    const fetchCity = async () => {
 
-      try {
+    try {
 
-        const data = await apiRequest(`/api/cities/${cityId}`);
-        setCity(data);
+      const data = await apiRequest(`/api/cities/${cityId}`);
+      setCity(data);
 
-        } catch (err) {
-          console.error("Ett fel inträffade vid fetch:", err);
-         
-        }
-    };
-    fetchCity();
+      } catch (err) {
+        console.error("Ett fel inträffade vid fetch:", err);
+
+      }
+  };
+  fetchCity();
 }, [cityId])
 
-    useEffect(() => {
-        const fetchZones = async () => {
-            const zonesData = await apiRequest(`/api/cities/${cityId}/zones`);
-            setZones(zonesData);
-            //console.log('zondata:', zonesData)
-        };
-        fetchZones();
-    }, [cityId]);
+  useEffect(() => {
+      const fetchZones = async () => {
+          const zonesData = await apiRequest(`/api/cities/${cityId}/zones`);
+          setZones(zonesData);
+          //console.log('zondata:', zonesData)
+      };
+      fetchZones();
+  }, [cityId]);
 
-    useEffect(() => {
-        const fetchBikes = async () => {
-            const bikesData = await apiRequest(`/api/cities/${cityId}/bikes`);
-            setBikes(bikesData);
-        };
-        fetchBikes();
-    }, [cityId]);
+  useEffect(() => {
+      const fetchBikes = async () => {
+          const bikesData = await apiRequest(`/api/cities/${cityId}/bikes`);
+          setBikes(bikesData);
+      };
+      fetchBikes();
+  }, [cityId]);
 
- if (!city) {
-    return <div>Hittar ingen stad med angivet Id.</div>;
+  if (!city) {
+    return <div>Hittar ingen stad med angivet ID.</div>;
   }
- return (
+
+return (
   <div className="city-container">
-    <h2 className="city-title">{city.name}</h2>
+    <h2>{city.name}</h2>
     <p><strong>Totalt antal cyklar i staden:</strong> {bikes.length}</p>
-    
-    <h3>Zoner</h3>
-    {zones.length > 0 ? (
-      <ul>
-        {zones.map(zone => {
-          const bikesInZone = bikes.filter(bike => bike.startingzone === zone._id);
+      
+    <h3>Laddstationer & Zoner</h3>
+      {zones.length > 0 ? (
+        <ul>
+          {zones.map(zone => {
+            const bikesInZone = bikes.filter(bike => bike.startingzone === zone._id);
           
-          return (
-            <li key={zone._id}>
-              <strong>{zone.name}</strong> (Zon: {zone.typeOfZone}, Antal cyklar: {bikesInZone.length})
-              
-              {bikesInZone.length > 0 && (
-                <ul>
-                  {bikesInZone.map(bike => {
-                    let statusText = bike.inUse ? 'Upptagen' : 'Ledig';
-                    if (bike.blocked) statusText = 'Blockad';
-                    if (bike.charging) statusText = 'Laddar';
-
-                    const mapsLink = `https://www.google.com/maps?q=${bike.position.latitude},${bike.position.longitude}`;
-                    return (
+            return (
+              <li key={zone._id}>
+                <strong>{zone.name}</strong> (Typ: {zone.typeOfZone}, Antal cyklar: {bikesInZone.length})
+                
+                {bikesInZone.length > 0 && (
+                  <ul>
+                    {bikesInZone.map(bike => (
+                      //Dispay bike info
                       <li key={bike._id}>
-                        Id: {bike._id} | Batteri: {bike.battery}% | Status: {statusText} |
-                        Position: {bike.position.latitude.toFixed(4)}(Y), {bike.position.longitude.toFixed(4)}(X)  |
-
-                        {/*adding rel"noopener noreferrer" to guard against refferer page being swapped for intrusion*/}
-                        <a href={mapsLink} target="_blank" rel="noopener noreferrer">
-                          Visa position
-                        </a>
+                        ID: {bike._id} | Batteri: {bike.battery}% | Status: {bike.inUse ? 'Uthyrd' : 'Ledig'}
                       </li>
-                    );
-                  })}
-                </ul>
-              )}
-            </li>
-          );
+                    ))}
+                  </ul>
+                )}
+              </li>
+            );
         })}
       </ul>
     ) : (
@@ -104,17 +91,17 @@ function City() {
       }
       return (
         <ul>
-          {freeparkedBikes.map(bike => {
+            {freeparkedBikes.map(bike => {
             let statusText = bike.inUse ? 'Upptagen' : 'Ledig';
             if (bike.blocked) statusText = 'Blockad';
 
-             const mapsLink = `https://www.google.com/maps?q=${bike.position.latitude},${bike.position.longitude}`;
+            const mapsLink = `https://www.google.com/maps?q=${bike.position.latitude},${bike.position.longitude}`;
 
             return (
               <li key={bike._id}>
                 Id: {bike._id} | Batteri: {bike.battery}% | Status: {statusText} |
                 Position: {bike.position.latitude.toFixed(4)}(Y), {bike.position.longitude.toFixed(4)}(X) |
-                 <a href={mapsLink} target="_blank" rel="noopener noreferrer">
+                <a href={mapsLink} target="_blank" rel="noopener noreferrer">
                   Hitta
                 </a>
               </li>
